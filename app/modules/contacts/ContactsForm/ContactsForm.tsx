2d6991695b7c408cbabb06403841596ab10fc398
rmena28 <ramon.e.mena@gmail.com>
2025-06-02 18:42:27 -0400

    feat: Refactor ContactsForm layout and add phone number validation

--- app/modules/contacts/ContactsForm/ContactsForm.tsx
+++ app/modules/contacts/ContactsForm/ContactsForm.tsx
@@ -12,6 +12,7 @@ import {
   Title,
   Center,
   SimpleGrid,
+  Divider,
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import {
@@ -21,6 +22,7 @@ import {
 } from "~/queries/contactsQueries";
 import type { Contact } from "~/models/ContactsModel";
 import classes from "./ContactsForm.module.css";
+import { IconDeviceFloppy } from "@tabler/icons-react";
 
 interface ContactsFormProps {
   mode: "create" | "edit";
@@ -47,6 +49,7 @@ export default function ContactsForm({
     validate: {
       firstName: (value) => (value ? null : "First name is required"),
       lastName: (value) => (value ? null : "Last name is required"),
+      phone: (value) => (value ? null : "Phone number is required"),
     },
   });
 
@@ -95,83 +98,87 @@ export default function ContactsForm({
 
   return (
     <form onSubmit={form.onSubmit(onSubmit)}>
-      <SimpleGrid cols={{ base: 1, sm: 2 }}>
-        <TextInput
-          label="First Name"
-          placeholder="Enter first name"
-          {...form.getInputProps("firstName")}
-          error={form.errors.firstName}
-          required
-        />
-        <TextInput
-          label="Last Name"
-          placeholder="Enter last name"
-          {...form.getInputProps("lastName")}
-          error={form.errors.lastName}
-          required
-        />
-        <TextInput
-          label="Identifier"
-          placeholder="Enter identifier"
-          {...form.getInputProps("identifier")}
-          error={form.errors.identifier}
-        />
-        <Select
-          label="Identifier Type"
-          placeholder="Select identifier type"
-          data={[
-            { value: "PERSONAL_ID", label: "National ID" },
-            {
-              value: "PASSPORT",
-              label: "Passport",
-            },
-            {
-              value: "DRIVER_LICENSE",
-              label: "Driver's License",
-            },
-          ]}
-          {...form.getInputProps("identifierType")}
-          error={form.errors.identifierType}
-          clearable
-        />
-        <DateInput
-          label="Birth Date"
-          placeholder="Pick birth date"
-          {...form.getInputProps("birthDate")}
-          error={form.errors.birthDate}
-          valueFormat="YYYY-MM-DD"
-          clearable
-        />
-        <TextInput
-          label="Address"
-          placeholder="Enter address"
-          {...form.getInputProps("address")}
-          error={form.errors.address}
-        />
-        <TextInput
-          label="Email"
-          placeholder="Enter email"
-          {...form.getInputProps("email")}
-          error={form.errors.email}
-          type="email"
-        />
-        <TextInput
-          label="Phone"
-          placeholder="Enter phone number"
-          {...form.getInputProps("phone")}
-          error={form.errors.phone}
-          type="tel"
-        />
+      <Stack gap={"xs"}>
+        <SimpleGrid cols={{ base: 1, sm: 2 }}>
+          <TextInput
+            label="First Name"
+            placeholder="Enter first name"
+            {...form.getInputProps("firstName")}
+            error={form.errors.firstName}
+            required
+          />
+          <TextInput
+            label="Last Name"
+            placeholder="Enter last name"
+            {...form.getInputProps("lastName")}
+            error={form.errors.lastName}
+            required
+          />
+          <TextInput
+            label="Identifier"
+            placeholder="Enter identifier"
+            {...form.getInputProps("identifier")}
+            error={form.errors.identifier}
+          />
+          <Select
+            label="Identifier Type"
+            placeholder="Select identifier type"
+            data={[
+              { value: "PERSONAL_ID", label: "National ID" },
+              {
+                value: "PASSPORT",
+                label: "Passport",
+              },
+              {
+                value: "DRIVER_LICENSE",
+                label: "Driver's License",
+              },
+            ]}
+            {...form.getInputProps("identifierType")}
+            error={form.errors.identifierType}
+            clearable
+          />
+          <DateInput
+            label="Birth Date"
+            placeholder="Pick birth date"
+            {...form.getInputProps("birthDate")}
+            error={form.errors.birthDate}
+            valueFormat="YYYY-MM-DD"
+            clearable
+          />
+          <TextInput
+            label="Address"
+            placeholder="Enter address"
+            {...form.getInputProps("address")}
+            error={form.errors.address}
+          />
+          <TextInput
+            label="Email"
+            placeholder="Enter email"
+            {...form.getInputProps("email")}
+            error={form.errors.email}
+            type="email"
+          />
+          <TextInput
+            label="Phone"
+            placeholder="Enter phone number"
+            {...form.getInputProps("phone")}
+            error={form.errors.phone}
+            type="tel"
+          />
+        </SimpleGrid>
+        <Divider />
         <Group justify="end" mt="md">
           <Button
             type="submit"
+            rightSection={<IconDeviceFloppy />}
             loading={createContact.isPending || updateContact.isPending}
             disabled={createContact.isPending || updateContact.isPending}
           >
             {mode === "edit" ? "Update" : "Create"}
           </Button>
         </Group>
-      </SimpleGrid>
+      </Stack>
     </form>
   );
 }
