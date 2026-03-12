7fbb7d60fa17978c8bf71defbfd5365c0ce0feb5
Enrique mota <enrique.motadejesus@gmail.com>
2026-03-12 18:00:49 -0400

    feat: add pronunciation dictionary rules and agent attachment

--- src/modules/configurations/ConfigurationsPage/ConfigurationsPage.tsx
+++ src/modules/configurations/ConfigurationsPage/ConfigurationsPage.tsx
@@ -9,6 +9,7 @@ import {
 	IconChartBar,
 	IconBook2,
 	IconChartBar,
+	IconBook2,
 } from '@tabler/icons-react';
 import { Outlet, useLocation, useNavigate } from 'react-router';
 import { useEffect } from 'react';
